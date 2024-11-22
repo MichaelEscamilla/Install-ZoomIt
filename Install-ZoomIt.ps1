9b149dc107a90f2d2878487b2175056cf5ac5f13
MichaelEscamilla <MichaelEscamilla@patchmypc.com>
2024-11-22 13:21:51 -0800

    Initial Release

---
+++ Install-ZoomIt.ps1
@@ -0,0 +1,165 @@
+<#
+.SYNOPSIS
+This script downloads and installs ZoomIt from Sysinternals Live, and can optionally configure various settings
+such as running on startup, accepting the EULA, hiding the system tray icon, and hiding the first run dialog.
+
+.DESCRIPTION
+This script downloads and installs ZoomIt from Sysinternals Live and currently saves the EXE to the User's Documents folder.
+Choose between the x86 and x64 versions of ZoomIt by specifying the Architecture parameter.
+The script can also configure various settings such as running ZoomIt on startup,
+accepting the EULA, hiding the system tray icon, and hiding the Options Window on first run.
+
+.PARAMETER AcceptEULA
+Specifies whether to accept the End User License Agreement (EULA) by creating a registry entry for EulaAccepted.
+This Prevents the EULA dialog from appearing on first run.
+
+.PARAMETER RunOnStartup
+Specifies whether to run ZoomIt on startup by adding a registry entry to the Current User's Run key.
+
+.PARAMETER ShowTrayIcon
+Specifies whether to show the ZoomIt icon in the system tray.
+
+.PARAMETER ShowFirstRun
+Specifies whether to show the Options Window on the first run.
+
+.PARAMETER Architecture
+Specifies the architecture of the ZoomIt executable to download. Valid values are "x64" and "x86".
+Default is "x64".
+
+.EXAMPLE
+Provide examples of how to use the script, including sample input and expected output.
+
+.NOTES
+Author: Michael Escamilla
+Date: 11/22/2024
+
+Version History:
+2024-11-22 - Initial release.
+
+Future Improvements:
+- Check if ZoomIt is already running on system
+- Check if zoomIt is already installed
+- Check if the installed version is the latest version
+- Add support for selecting a custom save path.
+- Add support for other ZoomIt settings.
+- Loggging maybe?
+#>
+param (
+    [ValidateSet("x64", "x86")]
+    [string]$Architecture = "x64",
+    [switch]$AcceptEULA,
+    [switch]$RunOnStartup,
+    [switch]$ShowTrayIcon,
+    [switch]$ShowFirstRun
+)
+
+### Check if connected to the internet, No Internet, No ZoomIt
+try {
+    $null = Test-Connection -ComputerName google.com -Count 1 -ErrorAction Stop
+}
+catch {
+    Write-Error "Not connected to the internet"
+    exit 1
+}
+
+### Set Download URL based on architecture
+if ($Architecture -eq "x64") {
+    $DownloadURL = "https://live.sysinternals.com/ZoomIt64.exe"
+}
+else {
+    $DownloadURL = "https://live.sysinternals.com/ZoomIt.exe"
+}
+
+### Parse File Name from Download URL
+$FileName = $DownloadURL.Split("/")[-1]
+
+### Set Save Path
+# This method will grab the OneDrive folder if Backup is enabled
+$SavePath = [Environment]::GetFolderPath('MyDocuments')
+
+### Combine Save Path and File Name
+$SaveFile = Join-Path -Path $SavePath -ChildPath $FileName
+
+### Download ZoomIt
+try {
+    Invoke-WebRequest -Uri $DownloadURL -OutFile $SaveFile -ErrorAction Stop
+}
+catch {
+    Write-Error "Failed to download ZoomIt from $DownloadURL"
+    exit 2
+}
+
+### Create Accept EULA Registry Key if AcceptEULA switch is set
+if ($AcceptEULA) {
+    # Define the registry path for ZoomIt settings
+    $RegPath = "HKCU:\Software\Sysinternals\ZoomIt"
+    
+    # Define the registry entry name for EULA acceptance
+    $RegName = "EulaAccepted"
+    
+    # Define the registry entry value to indicate EULA acceptance
+    $RegValue = "1"
+    
+    # Create the registry path if it doesn't exist
+    if (-not (Test-Path $RegPath)) {
+        New-Item -Path $RegPath -Force | Out-Null
+    }
+    
+    # Create or update the registry entry to accept the EULA
+    New-ItemProperty -Path $RegPath -Name $RegName -Value $RegValue -PropertyType DWord -Force | Out-Null
+}
+
+### Show First Run
+if (!($ShowFirstRun)) {
+    # Define the registry path for ZoomIt settings
+    $RegPath = "HKCU:\Software\Sysinternals\ZoomIt"
+    
+    # Define the registry entry name for first run
+    $RegName = "OptionsShown"
+    
+    # Define the registry entry value to indicate first run has been completed
+    $RegValue = "1"
+    
+    # Create the registry path if it doesn't exist
+    if (-not (Test-Path $RegPath)) {
+        New-Item -Path $RegPath -Force | Out-Null
+    }
+    
+    # Create or update the registry entry to indicate first run has been completed
+    New-ItemProperty -Path $RegPath -Name $RegName -Value $RegValue -PropertyType DWord -Force | Out-Null
+}
+
+### Remove from System Tray
+if (!($ShowTrayIcon)) {
+    # Define the registry path for ZoomIt settings
+    $RegPath = "HKCU:\Software\Sysinternals\ZoomIt"
+    
+    # Define the registry entry name for showing the system tray icon
+    $RegName = "ShowTrayIcon"
+    
+    # Define the registry entry value to hide the system tray icon
+    $RegValue = "0"
+    
+    # Create the registry path if it doesn't exist
+    if (-not (Test-Path $RegPath)) {
+        New-Item -Path $RegPath -Force | Out-Null
+    }
+    
+    # Create or update the registry entry to hide the system tray icon
+    New-ItemProperty -Path $RegPath -Name $RegName -Value $RegValue -PropertyType DWord -Force | Out-Null
+}
+
+### Run On Startup via Registry
+if ($RunOnStartup) {
+    # Define the registry path for startup programs
+    $RegPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
+    
+    # Define the registry entry name for ZoomIt
+    $RegName = "ZoomIt"
+    
+    # Define the registry entry value as the path to the ZoomIt executable
+    $RegValue = $SaveFile
+    
+    # Create or update the registry entry to run ZoomIt on startup
+    New-ItemProperty -Path $RegPath -Name $RegName -Value $RegValue -PropertyType String -Force | Out-Null
+}
--- README.md
+++ README.md
@@ -1,2 +1,112 @@
 # Install-ZoomIt
-Install ZoomIt from Sysinternals Live
+A powershell script to Install ZoomIt from Sysinternals Live to the Users Documents folder. Configure various setting to customize the behavior.
+
+## SYNTAX
+
+```powershell
+Install-ZoomIt.ps1 [-Architecture] [-AcceptEULA] [-RunOnStartup] [-ShowTrayIcon] [-ShowFirstRun]
+```
+## Description
+
+This script downloads and installs ZoomIt from Sysinternals Live and currently saves the EXE to the User's Documents folder.<br>
+Choose between the x86 and x64 versions of ZoomIt by specifying the Architecture parameter.<br>
+The script can also configure various settings such as running ZoomIt on startup,
+accepting the EULA, hiding the system tray icon, and hiding the Options Window on first run.
+
+## Sysinternal Registry Key
+Current User Registry Key where the ZoomIt settings are stored. These are the current 3 settings that are configurable using the script.
+![Sysinternals Registry Key - ZoomIt](/Images/Install-ZoomIt_SysinternalsKey-ZoomIt.png)
+
+## Current User Run Key
+Current User Run key. Regardless of what Architecutre EXE is used, the Value needs to be "ZoomIt" and the Data will be the path to the EXE file.
+![Current User Run Key - ZoomIt](/Images/Install-ZoomIt_CU-RunKey-ZoomIt.png)
+
+## Example
+
+### Example 1
+
+```powershell
+PS /> .\Install-ZoomIt.ps1 -RunOnStartup -AcceptEULA
+```
+
+## PARAMETERS
+
+### -Architecture
+
+Specify the architure of the ZoomIt file you want to download. Defaults to 'x64'
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases: None
+
+Required: False
+Position: 0
+Default value: x64
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -AcceptEULA
+
+Specifies whether to accept the End User License Agreement (EULA) by creating a registry value 'EulaAccepted' with the data value '1' in the key "HKCU:\Software\Sysinternals\ZoomIt". This Prevents the EULA dialog from appearing on first run.
+
+```yaml
+Type: Switch
+Parameter Sets: (All)
+Aliases: None
+
+Required: False
+Position: 1
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -RunOnStartup
+
+Specifies whether to run ZoomIt on startup by adding a registry entry to the Current User's Run key.
+
+```yaml
+Type: Switch
+Parameter Sets: (All)
+Aliases: None
+
+Required: False
+Position: 2
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -ShowTrayIcon
+
+Specifies whether to show the ZoomIt icon in the system tray. Controled by the Registry value "ShowTrayIcon" in the key "HKCU:\Software\Sysinternals\ZoomIt". 
+
+```yaml
+Type: Switch
+Parameter Sets: (All)
+Aliases: None
+
+Required: False
+Position: 2
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -ShowFirstRun
+
+Specifies whether to show the Options Window on the first run. Controled by the Registry value "OptionsShown" in the key "HKCU:\Software\Sysinternals\ZoomIt". 
+
+```yaml
+Type: Switch
+Parameter Sets: (All)
+Aliases: None
+
+Required: False
+Position: 2
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```

8b7f967ddbb0f9eb418935adeb17ac38fa2a15ca
MichaelEscamilla <MichaelEscamilla@protonmail.com>
2024-12-02 23:31:24 -0800

    2024.12.2.1

--- Install-ZoomIt.ps1
+++ Install-ZoomIt.ps1
@@ -1,3 +1,42 @@
+<#PSScriptInfo
+
+.VERSION 2024.12.2.1
+
+.GUID 7b9ffe16-dfb8-4b3a-97b1-2bc668ac5700
+
+.AUTHOR Michael Escamilla
+
+.COMPANYNAME
+
+.COPYRIGHT
+
+.TAGS
+
+.LICENSEURI
+
+.PROJECTURI
+
+.ICONURI
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS
+
+.EXTERNALSCRIPTDEPENDENCIES
+
+.RELEASENOTES
+2024.11.22.0    -   Initial release
+2024.11.22.1    -   Changed Parameter ShowFirstRun to ShowOptions
+2024.12.2.1     -   Added Check if ZoomIt is already in Destination Path
+                    Added Check if ZoomIt version is newer than the existing version in the Destination Path
+                    Added Check if Zoomit is already running on system
+                    Modified the ShowTrayIcon parameter to always set a value
+                    Added some functions for repeated tasks
+
+.PRIVATEDATA
+
+#> 
+
 <#
 .SYNOPSIS
 This script downloads and installs ZoomIt from Sysinternals Live, and can optionally configure various settings
@@ -8,45 +47,29 @@ This script downloads and installs ZoomIt from Sysinternals Live and currently s
 Choose between the x86 and x64 versions of ZoomIt by specifying the Architecture parameter.
 The script can also configure various settings such as running ZoomIt on startup,
 accepting the EULA, hiding the system tray icon, and hiding the Options Window on first run.
-
 .PARAMETER AcceptEULA
 Specifies whether to accept the End User License Agreement (EULA) by creating a registry entry for EulaAccepted.
 This Prevents the EULA dialog from appearing on first run.
-
 .PARAMETER RunOnStartup
 Specifies whether to run ZoomIt on startup by adding a registry entry to the Current User's Run key.
-
 .PARAMETER ShowTrayIcon
 Specifies whether to show the ZoomIt icon in the system tray. Will always set a value in the registry.
-
 .PARAMETER ShowOptions
 Specifies whether to show the Options Window on the first run.
-
 .PARAMETER Architecture
 Specifies the architecture of the ZoomIt executable to download. Valid values are "x64" and "x86".
 Default is "x64", I would recommend using the x64 version unless you have a specific reason to use the x86 version. The x86 version will run the x64 version from %TEMP% on a 64-bit system.
-
 .EXAMPLE
-Provide examples of how to use the script, including sample input and expected output.
-
+.\Install-ZoomIt.ps1 -AcceptEULA -RunOnStartup -ShowTrayIcon
 .NOTES
-Author: Michael Escamilla
-Date: 11/22/2024
-
-Version History:
-2024.11.22.0 -  Initial release
-2024.11.22.1 -  Changed Parameter ShowFirstRun to ShowOptions
-2024.12.2.0 -   Added Check if ZoomIt is already in Destination Path
-                Added Check if ZoomIt version is newer than the existing version in the Destination Path
-                Added Check if Zoomit is already running on system
-                Modified the ShowTrayIcon parameter to always set a value
-                Added some functions for repeated tasks
-
 Future Improvements:
-- Add support for selecting a custom save path.
+- Add support for setting a custom Save path.
+- Add support for setting a custom Destination Path.
 - Add support for other ZoomIt settings.
 - Loggging maybe?
+
 #>
+
 param (
     [ValidateSet("x64", "x86")]
     [string]$Architecture = "x64",
